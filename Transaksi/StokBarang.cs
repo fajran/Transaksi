8106d5dec84492a200384a8180d3b37d846d640e
Fajran Iman Rusadi <fajran@gmail.com>
2012-03-03 22:52:44 +0100

    Add StokBarang form

---
+++ Transaksi/StokBarang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Transaksi
+{
+    public partial class StokBarang : Form
+    {
+        public StokBarang()
+        {
+            InitializeComponent();
+        }
+    }
+}
