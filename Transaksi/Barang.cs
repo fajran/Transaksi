a09d83c7e6a316218993c78f83e27edd1a13b8f6
Fajran Iman Rusadi <fajran@gmail.com>
2012-03-04 15:31:47 +0100

    Add Barang and Stok data structure

---
+++ Transaksi/Barang.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transaksi
+{
+    class Barang
+    {
+        private string _nama;
+        private int _harga;
+        private int _stok;
+
+        public Barang(string nama) : this(nama, 0, 0)
+        {
+        }
+
+        public Barang(string nama, int harga) : this(nama, harga, 0)
+        {
+        }
+
+        public Barang(string nama, int harga, int stok)
+        {
+            _nama = nama;
+            _harga = harga;
+            _stok = stok;
+        }
+
+        public string Nama
+        {
+            set { _nama = value; }
+            get { return _nama; }
+        }
+
+        public int Harga
+        {
+            set { _harga = value; }
+            get { return _harga; }
+        }
+
+        public int Stok
+        {
+            set { _stok = value; }
+            get { return _stok; }
+        }
+    }
+}
